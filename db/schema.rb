0ea5de70e0fc34b4adad0e47803c88d96062bce9
Debbie <cinemaestro@yahoo.com>
2015-01-25 13:30:21 -0800

    add Active Record migration for adding due_date column to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125205920) do
+ActiveRecord::Schema.define(version: 20150125211739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150125205920) do
     t.boolean  "done"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "due_date"
   end
 
 end
