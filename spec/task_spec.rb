4d8a413d5f00b175ea330108bd7bcfde62abdf88
Debbie <cinemaestro@yahoo.com>
2015-01-25 15:18:55 -0800

    updates spec_helper and specs for Active Record and alters code to show only incomplete tasks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,32 +1,13 @@
 require('spec_helper')
 
 describe(Task) do
-  describe(".all") do
-    it("is empty at first") do
-      expect(Task.all()).to(eq([]))
-    end
-  end
-
-  describe("#save") do
-    it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL"})
-      test_task.save()
-      expect(Task.all()).to eq([test_task])
-    end
-  end
-
-  describe("#description") do
-    it("lets you give it a description") do
-      test_task = Task.new({:description => "learn SQL"})
-      expect(test_task.description()).to eq("learn SQL")
-    end
-  end
-
-  describe("#==") do
-    it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
-      expect(task1).to(eq(task2))
+  describe(".not_done") do
+    it("returns the incomplete tasks") do
+      not_done_task1 = Task.create(:description => "gotta do it", :done => false)
+      not_done_task2 = Task.create(:description => "gotta do it too", :done => false)
+      not_done_tasks = [not_done_task1, not_done_task2]
+      done_task = Task.create({:description => "done task", :done => true})
+      expect(Task.not_done()).to eq(not_done_tasks)
     end
   end
 end
